9205ea855cf5d6e2a6121df783fa0986b7015c08
karan4587 <karan@Karans-MacBook-Pro.local>
2015-07-27 16:20:23 +0530

    Setup Git repo

---
+++ iWardrobe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iWardrobe
+//
+//  Created by Karan on 27/07/15.
+//  Copyright (c) 2015 Karan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iWardrobe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iWardrobe
+//
+//  Created by Karan on 27/07/15.
+//  Copyright (c) 2015 Karan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
